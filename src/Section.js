67dd9febab172b73f88040709354dea7161f5fb8
Jono Cairns <jonocairns@gmail.com>
2017-10-08 20:26:57 +1300

    Content (#3)
    * hide css files vscode
    
    * some real content

--- src/Section.js
+++ src/Section.js
@@ -7,11 +7,14 @@ class Section extends Component {
         return (
             <div className="d-flex flex-column">
                 <div>
-                    <Heading title={this.props.title} />
+                    <Heading title={this.props.title}/>
                 </div>
                 {this.props.items.map((item, index) =>
-                    <div key={`item-${index}`}>
-                        <h5>{item.title}</h5>
+                    <div key={`item-${index}`} className="mb-4">
+                        {item.link ? 
+                        <a href={item.link}><h5>{item.title}</h5></a> : 
+                        <h5>{item.title}</h5>}
+                        
                         <p className="App-text">{item.subtitle}</p>
                         <ul className="p-0 m-0 pl-4 App-text">
                             {item.attributes.map((attr, index) =>
