35e705f48a3a73b82ec5978bbc728aca203bdb4d
ellie <elizabeth.h.earle@gmail.com>
2017-11-13 17:11:00 +1300

    adjusted margins and background colour for icons in heading

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -8,28 +8,32 @@ import content from './content.json';
 class Jumbotron extends Component {
     render() {
         return (
-            <div className="App-bg d-flex justify-content-center align-items-center mt-10">
-                <div id="home">
-                    {/* <h1 className="header-font">{content.title}</h1> */}
-                    <img src="https://i.imgur.com/HhO7DmJ.jpg" className="App-jumbotron-hero-image" alt="Ellie Earle: Creative Design & Marketing" />
-                    <ul className="App-bg--connect d-flex justify-content-between">
+            <div>
 
-                        <li className="p-3" role="button">
-                            <a href={`mailto:${content.contact.email}`}><img src={email} alt="email" /></a>
-                        </li>
-                        <li className="p-3" role="button">
-                            <a href={content.contact.social.linkedIn}><img src={ln} alt="linkedin" className="linkedin" /></a>
-                        </li>
-                        <li className="p-3"  role="button">
-                            <a target="_blank" href={`tel:${content.contact.phone}`}><img src={phone} alt="phone" /></a>
-                        </li>
-                        <li className="p-3 "  role="button">
-                            <a target="_blank" href={content.contact.social.vimeo}><img src={vimeo} width="46" alt="vimeo" /></a>
-                        </li>
-                    </ul>
+                <div className="App-bg d-flex justify-content-center align-items-center mt-10">
+                    <div id="home">
+                        {/* <h1 className="header-font">{content.title}</h1> */}
+                        <img src="https://i.imgur.com/HhO7DmJ.jpg" className="App-jumbotron-hero-image" alt="Ellie Earle: Creative Design & Marketing" />
+
+                    </div>
+
                 </div>
-            </div>
-        );
+                 <ul className="App-bg--connect d-flex justify-content-between pl-5 pr-5">
+
+                    <li className="p-4" role="button">
+                        <a href={`mailto:${content.contact.email}`}><img src={email} alt="email" /></a>
+                    </li>
+                    <li className="p-4" role="button">
+                        <a href={content.contact.social.linkedIn}><img src={ln} alt="linkedin" className="linkedin" /></a>
+                    </li>
+                    <li className="p-4" role="button">
+                        <a target="_blank" href={`tel:${content.contact.phone}`}><img src={phone} alt="phone" /></a>
+                    </li>
+                    <li className="p-4 " role="button">
+                        <a target="_blank" href={content.contact.social.vimeo}><img src={vimeo} width="46" alt="vimeo" /></a>
+                    </li>
+                </ul>
+            </div>);
     }
 }
 
