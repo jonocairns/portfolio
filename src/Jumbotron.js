ec1cc7589ac3579c130cbdd4403bbeaf195fabad
ellie <elizabeth.h.earle@gmail.com>
2017-11-15 19:23:52 +1300

    fixed icon and banners for both mobile and desktop, replaced favicon

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -13,23 +13,25 @@ class Jumbotron extends Component {
                 <div className="App-bg d-flex justify-content-center align-items-center mt-10">
                     <div id="home">
                         {/* <h1 className="header-font">{content.title}</h1> */}
-                        <img src="https://i.imgur.com/HhO7DmJ.jpg" className="App-jumbotron-hero-image" alt="Ellie Earle: Creative Design & Marketing" />
+                        <div className="App-jumbotron-hero-image" >
+                        
+                        </div>
 
                     </div>
 
                 </div>
                  <ul className="App-bg--connect d-flex justify-content-between pl-5 pr-5">
 
-                    <li className="p-4" role="button">
+                    <li className="p-1" role="button">
                         <a href={`mailto:${content.contact.email}`}><img src={email} alt="email" /></a>
                     </li>
-                    <li className="p-4" role="button">
+                    <li className="p-1" role="button">
                         <a href={content.contact.social.linkedIn}><img src={ln} alt="linkedin" className="linkedin" /></a>
                     </li>
-                    <li className="p-4" role="button">
+                    <li className="p-1" role="button">
                         <a target="_blank" href={`tel:${content.contact.phone}`}><img src={phone} alt="phone" /></a>
                     </li>
-                    <li className="p-4 " role="button">
+                    <li className="p-1 " role="button">
                         <a target="_blank" href={content.contact.social.vimeo}><img src={vimeo} width="46" alt="vimeo" /></a>
                     </li>
                 </ul>
